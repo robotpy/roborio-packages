6b5a8cbd63abe3d3506fd3e4e5dc0418eea8c4e5
Dustin Spicuzza <dustin@virtualroadside.com>
2015-12-27 20:51:20 -0500

    Allow nested packages

--- ipkg/Mk/pkgdata.mk
+++ ipkg/Mk/pkgdata.mk
@@ -3,4 +3,5 @@ VERSION ?= $(shell grep Version: control | cut -c 10-)
 ARCH ?= $(shell grep Architecture: control | cut -c 15-)
 SOURCE ?= $(shell grep Source: control | cut -c 9-)
 
-IPK_NAME = ../${PACKAGE}_${VERSION}_${ARCH}.ipk
+IPK_DEST ?= ..
+IPK_NAME = ${IPK_DEST}/${PACKAGE}_${VERSION}_${ARCH}.ipk
