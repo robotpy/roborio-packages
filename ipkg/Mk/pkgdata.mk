8452c78211eca9797032f14c693d7d623db67457
Dustin Spicuzza <dustin@virtualroadside.com>
2017-01-02 13:45:54 -0500

    Add BUILD_ROOT variable and fix ipk directory

--- ipkg/Mk/pkgdata.mk
+++ ipkg/Mk/pkgdata.mk
@@ -3,5 +3,8 @@ VERSION ?= $(shell grep Version: control | cut -c 10-)
 ARCH ?= $(shell grep Architecture: control | cut -c 15-)
 SOURCE ?= $(shell grep Source: control | cut -c 9-)
 
-IPK_DEST ?= ..
+mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
+BUILD_ROOT = $(abspath ${mkfile_path}/../..)
+
+IPK_DEST ?= ${BUILD_ROOT}
 IPK_NAME = ${IPK_DEST}/${PACKAGE}_${VERSION}_${ARCH}.ipk
