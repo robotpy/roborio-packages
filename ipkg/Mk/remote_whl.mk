eadf7e8f81920a8ff13e513b6a3a4c9959ba6dd4
Dustin Spicuzza <dustin@virtualroadside.com>
2017-01-10 10:10:15 -0500

    Update remote wheel support to use python 3.6

--- ipkg/Mk/remote_whl.mk
+++ ipkg/Mk/remote_whl.mk
@@ -4,13 +4,15 @@ mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 BUILD_ROOT = $(abspath ${mkfile_path}/../..)
 
 WHL_DEST ?= ${BUILD_ROOT}
-WHL_FNAME ?= ${PYPI_PACKAGE_NAME}-${PYPI_PACKAGE_VERSION}-cp35-cp35m-linux_armv7l.whl
+WHL_FNAME ?= ${PYPI_PACKAGE_NAME}-${PYPI_PACKAGE_VERSION}-cp36-cp36m-linux_armv7l.whl
 WHL_NAME ?= ${WHL_DEST}/${WHL_FNAME}
 
 ALLTARGETS ?= clean init-robotpy-opkg sync-date install-deps mkwheelhouse build whl
 
 BUILD_DIR ?= wheelhouse
-BUILD_CMD ?= /usr/local/bin/pip3 --disable-pip-version-check install wheel && /usr/local/bin/pip3 -v --disable-pip-version-check wheel --no-deps ${PYPI_PACKAGE_NAME}==${PYPI_PACKAGE_VERSION}
+BUILD_CMD ?= /usr/local/bin/pip3 --disable-pip-version-check install wheel && /usr/local/bin/pip3 -v --disable-pip-version-check wheel --no-deps -b . ${PYPI_PACKAGE_NAME}==${PYPI_PACKAGE_VERSION}
+
+DEPS ?= binutils-symlinks gcc-symlinks g++-symlinks libgcc-s-dev make python36-dev ${EXTRA_DEPS}
 
 include ${BUILD_ROOT}/Mk/remote_build.mk
 
