cace3a707d9f993c332a83bdf44966853ae9a1be
Dustin Spicuzza <dustin@virtualroadside.com>
2018-01-06 08:50:01 +0100

    Make script interactive

--- ipkg/promote.py
+++ ipkg/promote.py
@@ -10,20 +10,41 @@ ROOT = dirname(__file__)
 DEVROOT = join(ROOT, '2018-dev')
 PRODROOT = join(ROOT, '2018')
 
+def _plist():
+    idx = -1
+    for fn in sorted(glob.glob(join(DEVROOT, '*.ipk'))):
+        idx += 1
+        fn = basename(fn)
+        name, version, arch = fn.split('_')
+
+        yield name, version
+
+        if exists(join(PRODROOT, fn)):
+            print(idx, 'OK:', name, version)
+        else:
+            print(idx, "--:", name, version)
+
 @click.group()
 def cli():
     pass
 
 @cli.command(name='list')
 def _list():
-    for fn in sorted(glob.glob(join(DEVROOT, '*.ipk'))):
-        fn = basename(fn)
-        name, version, arch = fn.split('_')
+    list(_plist())
 
-        if exists(join(PRODROOT, fn)):
-            print('OK:', name, version)
-        else:
-            print("--:", name, version)
+@cli.command()
+def plist():
+    while True:
+        print('---')
+        l = list(_plist())
+        print('---')
+        idx = input('Which? ')
+        try:
+            idx = int(idx)
+        except ValueError:
+            break
+
+        _promote(*l[idx])
 
 
 @cli.command()
@@ -31,6 +52,9 @@ def _list():
 @click.argument('version')
 @click.option('-f', '--force', default=False)
 def promote(package, version, force):
+    _promote(package, version, force)
+
+def _promote(package, version, force=False):
     fname = '%s_%s_cortexa9-vfpv3.ipk' % (package, version)
     srcpath = join(DEVROOT, fname)
     dstpath = join(PRODROOT, fname)
